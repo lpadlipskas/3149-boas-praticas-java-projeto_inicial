d86019d02a73b6f50f35cf1db4882d43c68640bf
lpadlipskas <lpadlipskas@yahoo.com.br>
2025-05-21 19:43:00 -0300

    final aula 03

---
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -0,0 +1,50 @@
+package br.com.alura.domain;
+
+public class Abrigo {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Pet[] pets;
+
+    public Abrigo() {
+    }
+
+    public Abrigo(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+}
